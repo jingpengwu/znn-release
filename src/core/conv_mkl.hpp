a8308c8543d99c0faf8bb6a964c68a26187e2504
jingpengwu <jingpeng.wu@gmail.com>
2015-02-26 15:20:01 -0500

    remove redundant copy and computation

--- src/core/conv_mkl.hpp
+++ src/core/conv_mkl.hpp
@@ -48,16 +48,66 @@ inline double3d_ptr bf_conv_mkl(double3d_ptr ap, double3d_ptr bp)
     std::size_t ry = ay - by + 1;
     std::size_t rz = az - bz + 1;
 
+    double3d_ptr rp = volume_pool.get_double3d(rx,ry,rz);
+    double3d& r = *rp;
+
     // size
     MKL_INT input_shape[3]={ax,ay,az}, kernel_shape[3]={bx,by,bz};
+    MKL_INT rshape[3]={rx,ry,rz};
+
+    // give value
+    double* input=a.data();
+    double* kernel=b.data();
+
+    // 2d convolution using MKL
+    VSLConvTaskPtr task;
+    MKL_INT dims=3;
+    int status;
+    const int mode = VSL_CONV_MODE_DIRECT;//direct convolution
+    const int start[3]={bx-1,by-1,bz-1};
+
+    status = vsldConvNewTask(&task,mode,dims,input_shape, kernel_shape, rshape);
+    status = vslConvSetStart(task, start);
+    status = vsldConvExec(task,input,NULL,kernel,NULL, r.data() ,NULL);
+    status = vslConvDeleteTask(&task);
+
+    return rp;
+}
+
+
+inline double3d_ptr bf_conv_sparse_mkl( const double3d_ptr& ap,
+                                        const double3d_ptr& bp,
+                                        const vec3i& s)
+{
+    double3d& a = *ap;
+    double3d& b = *bp;
+
+    std::size_t ax = a.shape()[0];
+    std::size_t ay = a.shape()[1];
+    std::size_t az = a.shape()[2];
+
+    std::size_t bx = b.shape()[0];
+    std::size_t by = b.shape()[1];
+    std::size_t bz = b.shape()[2];
+
+    std::size_t rx = ax - bx + 1;
+    std::size_t ry = ay - by + 1;
+    std::size_t rz = az - bz + 1;
+
+    // size
+    //MKL_INT input_shape[3]={ax,ay,az}, kernel_shape[3]={bx,by,bz};
     MKL_INT tx = ax + bx - 1, ty = ay + by - 1, tz = az + bz - 1;
     MKL_INT tshape[3]={tx,ty,tz};
 
     // give value
     double* input=a.data();
     double* kernel=b.data();
-
     // temporal variable for MKL's long convolution
+
+    // get stride
+    int stride[3] = {s[0],s[1],s[2]};
+    MKL_INT input_shape[3]={ax/s[0],ay/s[1],az/s[2]}, kernel_shape[3]={bx/s[0],by/s[1],bz/s[2]};
+
     double t[tx*ty*tz];
 
     // 2d convolution using MKL
@@ -69,19 +119,21 @@ inline double3d_ptr bf_conv_mkl(double3d_ptr ap, double3d_ptr bp)
 
     status = vsldConvNewTask(&task,mode,dims,input_shape, kernel_shape, tshape);
     //status = vslConvSetStart(task, start);
-    status = vsldConvExec(task,input,NULL,kernel,NULL,t,NULL);
+    status = vsldConvExec(task, input, stride, kernel, stride, t, NULL);
     status = vslConvDeleteTask(&task);
 
     // extract the center vector
     double3d_ptr rp = volume_pool.get_double3d(rx,ry,rz);
     double3d& r = *rp;
-    for(int i = 0; i < rx; i++)
-        for(int j = 0; j < ry; j++)
+    for(int i = 0; i < rx; ++i)
+        for(int j = 0; j < ry; ++j)
             for (int k = 0; k < rz; ++k)
                 r[i][j][k] = t[(i+bx-1)*ty*tz+ (j+by-1)*tz + k+bz-1];
 
     return rp;
 }
+
+
 
 // 2D convolution using MKL
 inline double3d_ptr bf_conv_mkl_2d(double3d_ptr ap, double3d_ptr bp)
@@ -126,63 +178,6 @@ inline double3d_ptr bf_conv_mkl_2d(double3d_ptr ap, double3d_ptr bp)
     for(int i = 0; i < rx; i++)
         for(int j = 0; j < ry; j++)
             r[i][j][0] = t[(i+bx-1)*ty+ j+by-1];
-
-    return rp;
-}
-
-inline double3d_ptr bf_conv_sparse_mkl( const double3d_ptr& ap,
-                                        const double3d_ptr& bp,
-                                        const vec3i& s)
-{
-    double3d& a = *ap;
-    double3d& b = *bp;
-
-    std::size_t ax = a.shape()[0];
-    std::size_t ay = a.shape()[1];
-    std::size_t az = a.shape()[2];
-
-    std::size_t bx = b.shape()[0];
-    std::size_t by = b.shape()[1];
-    std::size_t bz = b.shape()[2];
-
-    std::size_t rx = ax - bx + 1;
-    std::size_t ry = ay - by + 1;
-    std::size_t rz = az - bz + 1;
-
-    // size
-    MKL_INT input_shape[3]={ax,ay,az}, kernel_shape[3]={bx,by,bz};
-    MKL_INT tx = ax + bx - 1, ty = ay + by - 1, tz = az + bz - 1;
-    MKL_INT tshape[3]={tx,ty,tz};
-
-    // give value
-    double* input=a.data();
-    double* kernel=b.data();
-    // temporal variable for MKL's long convolution
-
-    // get stride
-    int stride[3] = {s[0],s[1],s[2]};
-
-    double t[tx*ty*tz];
-
-    // 2d convolution using MKL
-    VSLConvTaskPtr task;
-    MKL_INT dims=3;
-    int status;
-    const int mode = VSL_CONV_MODE_DIRECT;//direct convolution
-    //const int start[3]={bx-1,by-1,bz-1};
-
-    status = vsldConvNewTask(&task,mode,dims,input_shape, kernel_shape, tshape);
-    //status = vslConvSetStart(task, start);
-    status = vsldConvExec(task, input, NULL, kernel, NULL, t, NULL);
-    status = vslConvDeleteTask(&task);
-
-    // extract the center vector
-    double3d_ptr rp = volume_pool.get_double3d(rx,ry,rz);
-    double3d& r = *rp;
-    for(int i = 0; i < rx; ++i)
-        for(int j = 0; j < ry; ++j)
-            for (int k = 0; k < rz; ++k)
-                r[i][j][k] = t[(i*s[0]+bx-1)*ty*tz+ (j*s[1]+by-1)*tz + k*s[2]+bz-1];
 
     return rp;
 }
--- znn_train.sh
+++ znn_train.sh
@@ -6,6 +6,6 @@
 
 export LD_LIBRARY_PATH="/opt/intel/composer_xe_2013_sp1.4.211/mkl/lib/intel64:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/usr/local/boost/1.55.0/lib64:$LD_LIBRARY_PATH"
-
+export LD_LIBRARY_PATH="/usr/local/fftw/gcc/openmpi-1.6.5/3.3.4/lib64/:$LD_LIBRARY_PATH"
 
 ./bin/znn_mkl --options="train.config"
